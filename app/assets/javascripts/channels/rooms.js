b9d3add92e61b6dfde9d5de47bcb17c543a2db1e
kortirso <kortirso@gmail.com>
2016-07-29 20:46:18 +0300

    disable channels at welcome page

--- app/assets/javascripts/channels/rooms.js
+++ app/assets/javascripts/channels/rooms.js
@@ -1,17 +1,18 @@
 $(function() {
     messages = $('#messages');
+    if(messages.length > 0) {
+        App.messages = App.cable.subscriptions.create({ channel: 'RoomsChannel', room_id: messages.data('room') }, {
+            received: function(data) {
+                $('#message_body').val('');
+                messages.append(this.renderMessage(data, messages.data('owner')));
+                $('.middle').animate({ 'scrollTop': $('.middle')[0].scrollHeight }, 'slow');
+            },
 
-    App.messages = App.cable.subscriptions.create({ channel: 'RoomsChannel', room_id: messages.data('room') }, {
-        received: function(data) {
-            $('#message_body').val('');
-            messages.append(this.renderMessage(data, messages.data('owner')));
-            $('.middle').animate({ 'scrollTop': $('.middle')[0].scrollHeight }, 'slow');
-        },
-
-        renderMessage: function(data, ownerName) {
-            if(ownerName == data.user) owner = 'users';
-            else owner = 'roommates';
-            return "<div class='message " + owner + "'><p class='author'>" + data.user + "</p><div class='body'><span class='time'>" + data.time + "</span><div class='text'>" + data.message + "</div></div></div>";
-        }
-    });
+            renderMessage: function(data, ownerName) {
+                if(ownerName == data.user) owner = 'users';
+                else owner = 'roommates';
+                return "<div class='message " + owner + "'><p class='author'>" + data.user + "</p><div class='body'><span class='time'>" + data.time + "</span><div class='text'>" + data.message + "</div></div></div>";
+            }
+        });
+    }
 });
