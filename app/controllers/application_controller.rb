3e7140515e6820ffef505e0be221525ab051dac3
kortirso <kortirso@gmail.com>
2016-08-01 07:07:54 +0300

    some fixes

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -3,6 +3,7 @@ class ApplicationController < ActionController::Base
     before_action :configure_permitted_parameters, if: :devise_controller?
 
     protected
+    
     def configure_permitted_parameters
         devise_parameter_sanitizer.permit(:sign_up, keys: [:username])
         devise_parameter_sanitizer.permit(:sign_in, keys: [:username])
@@ -10,6 +11,7 @@ class ApplicationController < ActionController::Base
     end
 
     private
+
     def get_access
         render template: 'welcome/index' unless current_user
     end
